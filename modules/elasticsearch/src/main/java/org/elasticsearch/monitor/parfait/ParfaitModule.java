d0aeba9c65ac83a797427706734a450bfe416297
psmith <psmith@aconex.com>
2011-05-06 14:17:12 +1000

    Simplify the creation of the SearchPhase metrics for code readability.

--- modules/elasticsearch/src/main/java/org/elasticsearch/monitor/parfait/ParfaitModule.java
+++ modules/elasticsearch/src/main/java/org/elasticsearch/monitor/parfait/ParfaitModule.java
@@ -2,18 +2,22 @@ package org.elasticsearch.monitor.parfait;
 
 import com.custardsource.parfait.spring.Profiled;
 import org.apache.commons.lang.StringUtils;
+import org.elasticsearch.common.aopalliance.intercept.MethodInterceptor;
 import org.elasticsearch.common.inject.AbstractModule;
 import org.elasticsearch.common.inject.matcher.Matcher;
 import org.elasticsearch.common.inject.matcher.Matchers;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.index.engine.Engine;
 import org.elasticsearch.index.engine.robin.RobinEngine;
+import org.elasticsearch.search.SearchPhase;
 import org.elasticsearch.search.dfs.DfsPhase;
 import org.elasticsearch.search.facet.FacetPhase;
 import org.elasticsearch.search.fetch.FetchPhase;
 import org.elasticsearch.search.query.QueryPhase;
 
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 
 public class ParfaitModule extends AbstractModule {
     private final Settings settings;
@@ -27,15 +31,11 @@ public class ParfaitModule extends AbstractModule {
         final ParfaitService parfaitService = new ParfaitService(settings);
         bind(ParfaitService.class).toInstance(parfaitService);
 
-        // TODO these eventGroups shoud be given to ParfaitService to register, it shouldn't really have the knowledge
-        bindInterceptor(Matchers.subclassesOf(QueryPhase.class), Matchers.annotatedWith(Profiled.class),
-                newProfiledMethodCounter(parfaitService, "elasticsearch.search.query.count", "Search Query phase counter", ParfaitService.SEARCH_EVENT_GROUP, "query"));
-        bindInterceptor(Matchers.subclassesOf(FetchPhase.class), Matchers.annotatedWith(Profiled.class),
-                newProfiledMethodCounter(parfaitService, "elasticsearch.search.fetch.count", "Search Fetch phase counter", ParfaitService.SEARCH_EVENT_GROUP, "fetch"));
-        bindInterceptor(Matchers.subclassesOf(FacetPhase.class), Matchers.annotatedWith(Profiled.class),
-                newProfiledMethodCounter(parfaitService, "elasticsearch.search.facet.count", "Search Facet phase counter", ParfaitService.SEARCH_EVENT_GROUP, "facet"));
-        bindInterceptor(Matchers.subclassesOf(DfsPhase.class), Matchers.annotatedWith(Profiled.class),
-                newProfiledMethodCounter(parfaitService, "elasticsearch.search.dfs.count", "Search DFS phase counter", ParfaitService.SEARCH_EVENT_GROUP, "dfs"));
+        List<Class<? extends SearchPhase>> searchPhasesToInstrument = Arrays.asList(QueryPhase.class, FetchPhase.class, FacetPhase.class, DfsPhase.class);
+
+        for (Class searchPhase: searchPhasesToInstrument) {
+            bindSearchPhaseExecutionToCounterAndAction(parfaitService, searchPhase);
+        }
 
         try {
             bindEngineMethodToCounterAndAction(parfaitService, RobinEngine.class.getMethod("create", Engine.Create.class));
@@ -51,6 +51,20 @@ public class ParfaitModule extends AbstractModule {
         }
     }
 
+    private void bindSearchPhaseExecutionToCounterAndAction(ParfaitService parfaitService, Class<QueryPhase> queryPhaseClass) {
+        String name = nameForSearchPhase(queryPhaseClass);
+        bindInterceptor(Matchers.subclassesOf(queryPhaseClass), Matchers.annotatedWith(Profiled.class),
+                newProfiledMethodCounter(parfaitService, String.format("elasticsearch.search.%s.count", name), String.format("Search %s phase counter", StringUtils.capitalize(name)), ParfaitService.SEARCH_EVENT_GROUP, name));
+    }
+
+    private MethodInterceptor newProfiledMethodCounter(ParfaitService parfaitService, String counterName, String counterDescription, String eventGroup, String action) {
+        return new ProfiledMethodCounter(parfaitService.getEventTimer().getCollector(), parfaitService.createMoniteredCounter(counterName, counterDescription), eventGroup, action);
+    }
+
+    private String nameForSearchPhase(Class<QueryPhase> queryPhaseClass) {
+        return StringUtils.removeEnd(StringUtils.lowerCase(queryPhaseClass.getSimpleName()), "phase");
+    }
+
 
     private void bindEngineMethodToCounterAndAction(ParfaitService parfaitService, Method method) {
         String methodName = method.getName();
@@ -64,14 +78,14 @@ public class ParfaitModule extends AbstractModule {
 
     private void bindEngineMethodToCounterAndAction(ParfaitService parfaitService, Matcher<Object> methodMatcher, String counterName, String counterDescription, String eventGroup, String action) {
         Class<Engine> clazz = Engine.class;
-        bindClassMethodToCounterAndAction(parfaitService, clazz, methodMatcher, counterName, counterDescription, eventGroup, action);
+        bindClassMethodToCounterAndAction(parfaitService, clazz, methodMatcher, eventGroup, action);
     }
 
-    private void bindClassMethodToCounterAndAction(ParfaitService parfaitService, Class<?> clazz, Matcher<Object> methodMatcher, String counterName, String counterDescription, String eventGroup, String action) {
-        bindInterceptor(Matchers.subclassesOf(clazz), methodMatcher, newProfiledMethodCounter(parfaitService, counterName, counterDescription, eventGroup, action));
+    private void bindClassMethodToCounterAndAction(ParfaitService parfaitService, Class<?> clazz, Matcher<Object> methodMatcher, String eventGroup, String action) {
+        bindInterceptor(Matchers.subclassesOf(clazz), methodMatcher, newProfiledShardBasedMethodCounter(parfaitService, eventGroup, action));
     }
 
-    private ProfiledShardBasedMethodCounter newProfiledMethodCounter(ParfaitService parfaitService, String name, String description, String eventGroup, String action) {
+    private ProfiledShardBasedMethodCounter newProfiledShardBasedMethodCounter(ParfaitService parfaitService, String eventGroup, String action) {
         return new ProfiledShardBasedMethodCounter(parfaitService, eventGroup, action);
     }
 }
