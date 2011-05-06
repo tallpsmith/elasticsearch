e0d47ed21139305ea3efe73e13e03a0ca44049a2
psmith <psmith@aconex.com>
2011-05-06 13:17:13 +1000

    Bind all the RobinEngine core actions to counters and the eventTimer, not quite a true DSL than I was originally thinking but at least it's a bit more readable now.

--- modules/elasticsearch/src/main/java/org/elasticsearch/monitor/parfait/ParfaitModule.java
+++ modules/elasticsearch/src/main/java/org/elasticsearch/monitor/parfait/ParfaitModule.java
@@ -1,13 +1,13 @@
 package org.elasticsearch.monitor.parfait;
 
 import com.custardsource.parfait.spring.Profiled;
+import org.apache.commons.lang.StringUtils;
 import org.elasticsearch.common.inject.AbstractModule;
 import org.elasticsearch.common.inject.matcher.Matcher;
 import org.elasticsearch.common.inject.matcher.Matchers;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.index.engine.Engine;
 import org.elasticsearch.index.engine.robin.RobinEngine;
-import org.elasticsearch.index.shard.service.IndexShard;
 import org.elasticsearch.search.dfs.DfsPhase;
 import org.elasticsearch.search.facet.FacetPhase;
 import org.elasticsearch.search.fetch.FetchPhase;
@@ -44,16 +44,40 @@ public class ParfaitModule extends AbstractModule {
         // tODO the annotations need to be on the subclass, and not the Engine class for some reason, need to understand why
         // TODO the log output has "elasticsearch:index" as the op name, that clearly should be "index:delete" (same for search, "search:query" and "search:fetch" etc..
         try {
-            Method delete = RobinEngine.class.getMethod("delete", Engine.Delete.class);
-            Matcher<Object> deleteMethodMatcher = Matchers.only(delete);
-            bindInterceptor(Matchers.subclassesOf(Engine.class), deleteMethodMatcher, newProfiledMethodCounter(parfaitService, "elasticsearch.index.delete.count", "Delete Index Operations", ParfaitService.INDEX_EVENT_GROUP, "delete"));
+            bindEngineMethodToCounterAndAction(parfaitService, RobinEngine.class.getMethod("create", Engine.Create.class));
+            bindEngineMethodToCounterAndAction(parfaitService, RobinEngine.class.getMethod("index", Engine.Index.class));
+            bindEngineMethodToCounterAndAction(parfaitService, RobinEngine.class.getMethod("flush", Engine.Flush.class));
+            bindEngineMethodToCounterAndAction(parfaitService, RobinEngine.class.getMethod("delete", Engine.Delete.class));
+            bindEngineMethodToCounterAndAction(parfaitService, RobinEngine.class.getMethod("delete", Engine.DeleteByQuery.class));
+            bindEngineMethodToCounterAndAction(parfaitService, RobinEngine.class.getMethod("optimize", Engine.Optimize.class));
+            bindEngineMethodToCounterAndAction(parfaitService, RobinEngine.class.getMethod("refresh", Engine.Refresh.class));
+            bindEngineMethodToCounterAndAction(parfaitService, RobinEngine.class.getMethod("snapshot", Engine.SnapshotHandler.class));
         } catch (NoSuchMethodException e) {
             throw new RuntimeException(e);
         }
     }
 
+
+    private void bindEngineMethodToCounterAndAction(ParfaitService parfaitService, Method method) {
+        String methodName = method.getName();
+        bindEngineMethodToCounterAndAction(parfaitService, method, String.format("elasticsearch.index.%s.count", methodName), StringUtils.capitalize(methodName) + " Index Operations");
+
+    }
+
+    private void bindEngineMethodToCounterAndAction(ParfaitService parfaitService, Method method, String counterName, String counterDescription) {
+        bindEngineMethodToCounterAndAction(parfaitService, Matchers.only(method), counterName, counterDescription, ParfaitService.INDEX_EVENT_GROUP, method.getName());
+    }
+
+    private void bindEngineMethodToCounterAndAction(ParfaitService parfaitService, Matcher<Object> methodMatcher, String counterName, String counterDescription, String eventGroup, String action) {
+        Class<Engine> clazz = Engine.class;
+        bindClassMethodToCounterAndAction(parfaitService, clazz, methodMatcher, counterName, counterDescription, eventGroup, action);
+    }
+
+    private void bindClassMethodToCounterAndAction(ParfaitService parfaitService, Class<?> clazz, Matcher<Object> methodMatcher, String counterName, String counterDescription, String eventGroup, String action) {
+        bindInterceptor(Matchers.subclassesOf(clazz), methodMatcher, newProfiledMethodCounter(parfaitService, counterName, counterDescription, eventGroup, action));
+    }
+
     private ProfiledMethodCounter newProfiledMethodCounter(ParfaitService parfaitService, String name, String description, String eventGroup, String action) {
         return new ProfiledMethodCounter(parfaitService.getEventTimer().getCollector(), parfaitService.createMoniteredCounter(name, description), eventGroup, action);
     }
-
 }
